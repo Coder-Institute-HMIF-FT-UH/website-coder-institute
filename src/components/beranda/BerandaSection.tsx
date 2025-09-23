6a7cc32fe70e9a205cb894114daa7503174e5cd2
fajri-fard <fajrijry14@gmail.com>
2025-09-23 20:44:51 +0800

    feat(faq): implement FAQ section with dynamic items and smooth scrolling (dummy data)

--- src/components/beranda/BerandaSection.tsx
+++ src/components/beranda/BerandaSection.tsx
@@ -1,10 +1,18 @@
-import { UnderConstruction } from '../common/UnderConstruction';
+import { FAQSection } from './FAQ/FAQSection';
 
 const BerandaSection = () => {
   return (
-    <>
-      <UnderConstruction />
-    </>
+    <div className="flex min-h-[80vh] flex-col items-center justify-center space-y-5 text-center">
+      {/* hero section */}
+      {/* tentang kami section */}
+      {/* visi kami section */}
+      {/* misi kami section */}
+      {/* prestasi terbaru section */}
+      {/* project terbaru section */}
+      {/* apa kata mereka section */}
+      <FAQSection />
+      {/* <UnderConstruction /> */}
+    </div>
   );
 };
 

c63beaa426c40f69b36a142e4e9e419473b023d6
fajri-fard <fajrijry14@gmail.com>
2025-09-24 00:53:50 +0800

    feat(beranda): add HeroSection responsive layout

--- src/components/beranda/BerandaSection.tsx
+++ src/components/beranda/BerandaSection.tsx
@@ -1,9 +1,11 @@
+import '@/styles/beranda.css';
 import { FAQSection } from './FAQ/FAQSection';
+import { HeroSection } from './hero/HeroSection';
 
 const BerandaSection = () => {
   return (
-    <div className="flex min-h-[80vh] flex-col items-center justify-center space-y-5 text-center">
-      {/* hero section */}
+    <div className="mb-20 flex min-h-[80vh] flex-col items-center justify-center gap-20 text-center">
+      <HeroSection />
       {/* tentang kami section */}
       {/* visi kami section */}
       {/* misi kami section */}

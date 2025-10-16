40cbbe8d85a8b198aeba9fa4185ba44b1760b8bc
fajri-fard <fajrijry14@gmail.com>
2025-10-17 02:50:31 +0800

    refactor(beranda/prestasi): edit prestasi data for data diversity

--- src/data/beranda/prestasiTerbaruData.ts
+++ src/data/beranda/prestasiTerbaruData.ts
@@ -9,33 +9,36 @@ export const prestasiTerbaruData: PrestasiItem[] = [
   {
     id: 1,
     title:
-      'Juara 3 pada PsychoBoost! Vol. 2: Essay and Olympic National Competition',
-    participants: 'Zulfahmi dan Adrian',
-    image: '/beranda/prestasi/psychoboost.png',
-  },
-  {
-    id: 2,
-    title:
-      'Juara 1 pada HackFest 2025 Regional Makassar oleh Google Developer Groups',
+      'Juara 1 HackFest 2025 Regional Makassar oleh Google Developer Groups',
     participants: 'Aulia Tifani, Firman Jaya Kusuma, dan Tasya Aulia Putri',
     image: '/beranda/prestasi/hackfest.png',
   },
   {
+    id: 2,
+    title:
+      'Juara 3 PsychoBoost! Vol. 2: Essay & Olympic National Competition Bidang Teknologi',
+    participants: 'Zulfahmi dan Adrian',
+    image: '/beranda/prestasi/psychoboost.png',
+  },
+  {
     id: 3,
-    title: 'Finalis Gemastik XVIII Bidang Pengembangan Aplikasi Permainan',
+    title:
+      'Finalis Gemastik XVIII Bidang Pengembangan Aplikasi Permainan (Game Development)',
     participants: 'Salsabila, Arief Nurhalim, dan Rizky Maulana',
     image: '/beranda/prestasi/gemastik-xviii.png',
   },
   {
     id: 4,
-    title: 'Finalis Gemastik XVIII Bidang Pengembangan Aplikasi Permainan',
-    participants: 'Salsabila, Arief Nurhalim, dan Rizky Maulana',
+    title:
+      'Top 10 Finalis National UI/UX Design Competition 2025 Universitas Gadjah Mada',
+    participants: 'Nabila Sari dan Iqbal Fadillah',
     image: '/beranda/prestasi/gemastik-xviii.png',
   },
   {
     id: 5,
-    title: 'Finalis Gemastik XVIII Bidang Pengembangan Aplikasi Permainan',
-    participants: 'Salsabila, Arief Nurhalim, dan Rizky Maulana',
+    title:
+      'Juara Harapan 1 IoT Innovation Challenge 2025 oleh Telkom Indonesia',
+    participants: 'Fathur Rahman, Rafiq Akbar, dan Indah Lestari',
     image: '/beranda/prestasi/gemastik-xviii.png',
   },
 ];

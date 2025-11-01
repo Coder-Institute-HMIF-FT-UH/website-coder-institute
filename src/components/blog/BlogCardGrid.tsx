3e1ffc0cdb5b4d8f741bce9553bb8814adc59675
fajri-fard <fajrijry14@gmail.com>
2025-11-02 03:47:02 +0800

    fix(blog): adjust horizontal padding and resolve horizontal scroll issue

--- src/components/blog/BlogCardGrid.tsx
+++ src/components/blog/BlogCardGrid.tsx
@@ -3,10 +3,12 @@ import { blogData } from '@/data/blog/blogData';
 
 const BlogCardGrid = () => {
   return (
-    <div className="container mt-16 grid w-full grid-cols-1 gap-8 md:grid-cols-2">
-      {blogData.map(blog => (
-        <Card key={blog.id} {...blog} />
-      ))}
+    <div className="blog-container mt-16">
+      <div className="grid w-full grid-cols-1 gap-8 md:grid-cols-2">
+        {blogData.map(blog => (
+          <Card key={blog.id} {...blog} />
+        ))}
+      </div>
     </div>
   );
 };
